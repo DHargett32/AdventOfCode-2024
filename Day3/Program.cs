9daade55a5a35c346c0b5bc01b22b66da41ae593
DHargett32 <dhargett@uab.edu>
2024-12-09 21:43:04 -0500

    In Progress

---
+++ Day3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadInputFile();
+    }
+
+    private static void ReadInputFile()
+    {
+        StreamReader file = new StreamReader(@"./../../../sample-input.txt");
+
+        string inputFileToParse = file.ReadToEnd();
+
+        string pattern = @"[A-Za-z]+\([^)]*\)";
+        Regex r = new Regex(pattern);
+        var matches = r.Matches(inputFileToParse).ToList();
+
+        string[] separators = { "mul(", ",", ")" };
+        matches.ForEach(i => i.Value.Split(separators,StringSplitOptions.None));
+        var numbers = matches.Where(i => Int32.Parse(i.Value) >= 0);
+            
+        // mul(123,456)
+    }
+}
